322ed682886f87d3ca0804ad3cef993d18a6a7ce
Your Name <you@example.com>
2021-07-02 20:08:46 +0300

    New experiments

--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from sklearn.decomposition import PCA
+from scipy.spatial.distance import pdist, cdist, squareform
 import numpy as np 
 from numpy import genfromtxt
 from numpy import linalg as LA
@@ -8,14 +9,79 @@ warnings.filterwarnings('ignore')
 from collections import defaultdict, Counter
 from tqdm import tqdm_notebook
 import matplotlib.pyplot as plt
+from matplotlib import cm, colors
 from sklearn.base import BaseEstimator
 from sklearn.neighbors import KNeighborsRegressor, NearestNeighbors
-from sklearn.metrics import silhouette_score, davies_bouldin_score, pairwise_distances
+from sklearn.metrics import silhouette_score, davies_bouldin_score, pairwise_distances, make_scorer
 from sklearn.model_selection import KFold
+from sklearn.manifold import TSNE
+from itertools import combinations
 from copy import copy
+from numba import cuda, jit, njit, prange, vectorize
+from sklearn.model_selection import cross_val_score, cross_validate, ParameterGrid, GridSearchCV
+from sklearn.multioutput import MultiOutputRegressor, RegressorChain, RegressorMixin
+from coranking import coranking_matrix
+from coranking.metrics import continuity, LCMC, trustworthiness
 
-def mae_score(y_pred, y):
-    return (np.linalg.norm(y_pred - y, axis=1, ord=1)/ (np.linalg.norm(y, axis=1, ord=1) + 1e-10)).mean()
+RANDOM_SEED=42
+FIGSIZE=(5,5)
+DPI=150
+FONTSIZE=12
+
+SILHOETTE_THRESHOLD_DISTINCT = 0.7
+DB_THRESHOLD_DISTINCT = 0.4
+
+def transform(method, est, X, parameters, scorer):
+    model_inst = method(n_components=2, **parameters, n_jobs=-1)
+    X_trans = model_inst.fit_transform(X)
+    if np.isnan(X_trans).any():
+        return np.inf
+    else:
+        cv_score = -cross_val_score(est, X_trans, X, scoring=scorer, cv=3, n_jobs=-1).mean()
+        return cv_score
+
+    
+def NN_ratio_score(X,y, k=5):
+    X_pd = squareform(pdist(X))
+    s = []
+    s_min = []
+    for y_i in np.unique(y):
+        mask = y==y_i
+        nearest_mates = np.sort(X_pd[mask,:][:,mask], axis=1)[:,1:k+1].mean(1)
+        nearest_foes = np.sort(X_pd[mask,:][:,~mask], axis=1)[:,1:k+1].mean(1) + 1e-5
+
+    #     y_i_diam = X_pd[mask,:][:,mask].min() + 1e-5
+    #     y_ij_r = X_pd[mask,:][:,~mask].max() + 1e-5
+
+    #     s_min.append(y_ij_r/y_i_diam)
+
+        s.append((nearest_mates/nearest_foes).mean())
+    return np.mean(s)# / max(s_min)
+    
+# def nearest_ratio_score(X,y, n_neighbors=5):
+#     nn = NearestNeighbors(n_neighbors=n_neighbors)
+#     nn.fit(X)
+#     neigh_dist, neigh_ind = nn.kneighbors(X)
+#     s = []
+#     for y1,y2 in list(combinations(np.unique(y),2)):
+#         # take two different clusters
+#         dist_C0 = neigh_dist[:,1:][y==y1].mean(1) # ср расстояние до ближайшей из своего кластера
+#         dist_C1 = neigh_dist[:,1:][y==y2].mean(1) # ср расстояние до ближайшей из своего кластера
+    
+#         cdist_C = cdist(X[y==y1],X[y==y2])
+        
+#         cdist_C.sort(axis=1)
+#         cdist_C0 = cdist_C[:,:n_neighbors].mean(axis=1) # ср расстояние до ближайшей из чужого кластера
+#         cdist_C.sort(axis=0)
+#         cdist_C1 = cdist_C[:n_neighbors].mean(axis=0) # ср расстояние до ближайшей из чужого кластера
+        
+#         s0 = (dist_C0/cdist_C0).mean()
+#         s1 = (dist_C1/cdist_C1).mean()
+#         s.append((s0 + s1)/2)
+#     return np.mean(s)
+
+def norm_entropy(x):
+    return -(x*np.log(x)).sum() / len(x)
 
 def unpack_data(paths):
     datasets = {}
@@ -27,6 +93,60 @@ def unpack_data(paths):
         except:
             datasets[label] = np.load(path, allow_pickle=True)
     return datasets
+
+def mae_score(y_pred, y):
+    return (np.linalg.norm(y_pred - y, axis=1, ord=1)/ (np.linalg.norm(y, axis=1, ord=1) + 1e-7)).mean()
+
+def unpack_data(paths):
+    datasets = {}
+    for path in paths:
+        label = path.split('/')[-1].split('.')[0].split('_')[:3]
+        label = '_'.join(label)
+        try:
+            datasets[label] = np.genfromtxt(path, delimiter=';')
+        except:
+            datasets[label] = np.load(path, allow_pickle=True)
+    return datasets
+
+def plot_results(DISTINCT_CLUSTERS_RESULTS, data, ml_method_name):
+    
+    for label, preds_dict in DISTINCT_CLUSTERS_RESULTS.items():
+        for method_name, (preds, n_clusters) in preds_dict.items():
+            dataset = data[label]
+            dataset_name = label.split('_')[0]
+            tax_name = label.split('_')[-1].upper()
+            # TSNE
+            N = min(dataset.shape[1], 5)
+#             fig, axes = plt.subplots(ncols=N+1, nrows=1, figsize=(5*(N+1), 5), dpi=280)
+            
+            tsne2 = TSNE(2, init='pca', verbose=0, angle=0.3, perplexity=20, n_iter=500, random_state=RANDOM_SEED)
+            tsne2.fit(dataset)
+            embedding = tsne2.embedding_
+
+            colors = [cm.get_cmap('viridis')(it) for it in np.linspace(0,1,n_clusters)]
+#             for c_number in range(n_clusters):
+#                 mask = preds[n_clusters] == c_number
+#                 perc = round(mask.sum()/len(mask),3)
+#                 axes[0].scatter(embedding[:,0][mask], embedding[:,1][mask], alpha=0.3, c=colors[c_number], label=f'cluster {c_number+1}, {perc}% of data')
+#             axes[0].set_title(f'Dataset: {dataset_name}, Tax: {tax_name}, Maifold Learning method: LLE \n t-SNE 2D visualization, {method_name} clustering to {n_clusters} clusters',fontsize=FONTSIZE)
+#             axes[0].legend()
+            # PROJECTIONS
+            plt.figure(figsize=FIGSIZE, dpi=DPI)
+            for c_number in range(n_clusters):
+                mask = preds[n_clusters] == c_number
+                perc = round(mask.sum()/len(mask),3)
+                plt.scatter(embedding[:,0][mask], embedding[:,1][mask], alpha=0.3, c=colors[c_number], label=f'cluster {c_number+1}, {perc}% of data')
+            plt.title(f'Dataset: {dataset_name}, Tax: {tax_name}, Maifold Learning method: {ml_method_name} \n t-SNE 2D visualization, {method_name} clustering to {n_clusters} clusters',fontsize=FONTSIZE)
+            plt.legend()
+            plt.show()
+#             for i,(d1,d2) in enumerate(list(combinations(np.arange(N),2))[:N]):
+#                 axes[i+1].scatter(dataset[:,d1], dataset[:,d2], alpha=0.1, c=preds[n_clusters])
+#                 axes[i+1].set_title(f'{dataset_name}, DIMS:{d1,d2}, {method_name} clustering to n_clust={n_clusters}')
+#             clear_output()
+#             plt.tight_layout()
+#             plt.show()
+
+
 
 def get_report(methods_dict, 
                cluster_results_list, 
@@ -63,10 +183,18 @@ def filter_paths(paths, keywords=[]):
         paths_filtered += list(filter(lambda x: word in x.split("/")[-1].split(".")[0], paths))
     return paths_filtered
 
-def get_neigh_perc(data, perc=95):
+@njit
+def js(p,q):
+    EPS = 1e-10
+    dkl_pq = np.sum(p * np.log((p+EPS)/(q + EPS)))
+    dkl_qp = np.sum(q * np.log((q+EPS)/(p + EPS)))
+    J = (dkl_pq + dkl_qp)/2
+    return J
+
+def get_neigh_perc(data, perc=95, metric='minkowski'):
     perc_list = []
     for n_neighbors in np.arange(3, 30, 1):
-        nn = NearestNeighbors(n_neighbors=n_neighbors)
+        nn = NearestNeighbors(n_neighbors=n_neighbors, metric=metric)
         nn.fit(data)
         neigborhood_X_dist, neigborhood_X_ind = nn.kneighbors(data, n_neighbors=n_neighbors)
         mean_neigh_distances = neigborhood_X_dist[:,1:].mean(1)
@@ -117,8 +245,7 @@ def create_clustering_pivot_table(results_list, methods_names, datasets_names, m
     return X_dbind, X_silh
 
 
-
-def clustering(datasets_dict, method_class, param_range, dbscan=False, dbscan_params_dict=None):
+def clustering(datasets_dict, method_class, param_range, precomputed=False, dbscan=False, dbscan_params_dict=None):
     # performing clustering
     cluster_metrics = defaultdict(dict)
     cluster_results = defaultdict(dict)
@@ -133,12 +260,26 @@ def clustering(datasets_dict, method_class, param_range, dbscan=False, dbscan_pa
             method = method_class(p)
             pred = method.fit_predict(dataset)
             if max(pred) > 0: # at least 2 clusters: [0,1]
-                centers = cl_centers(dataset, pred)
+                
                 ind = davies_bouldin_score(dataset, pred)
+                nnrs = NN_ratio_score(dataset,pred)
                 silh = silhouette_score(dataset, pred)
-                n = len(np.unique(pred[pred != -1]))
+                
+                unique_clusters = np.unique(pred[pred != -1])
+                n = len(unique_clusters)
+                cl_dist = np.ones(n)
+                for i,cl_number in enumerate(unique_clusters):
+                    cl_dist[i] = sum(pred == cl_number)/len(pred)
+                
                 noise_ratio = sum(pred == -1)/len(pred)
-                cluster_metrics[label][n] = [ind, silh, noise_ratio]
+                norm_entropy_ = norm_entropy(cl_dist)
+                
+                if n in cluster_metrics[label]:
+                    # same clustering with lower silhoette score
+                    if silh < cluster_metrics[label][n][1]:
+                        continue
+                        
+                cluster_metrics[label][n] = [ind, silh, noise_ratio, norm_entropy_, nnrs] # ,nrs
                 cluster_results[label][n] = pred
             else:
                 pass
@@ -146,7 +287,7 @@ def clustering(datasets_dict, method_class, param_range, dbscan=False, dbscan_pa
     return cluster_metrics, cluster_results
 
 
-def plot_proj_clustering(clustering_results, method='', suptitle=None, data_default_metrics=None):
+def plot_clustering(clustering_results, method='', suptitle=None, data_default_metrics=None):
     '''
     clustering_results - dict
     '''
@@ -155,23 +296,38 @@ def plot_proj_clustering(clustering_results, method='', suptitle=None, data_defa
     
     for label,data in results.items():
         if len(data) > 0:
-            plt.figure()
+            plt.figure(figsize=FIGSIZE, dpi=DPI)
             df = pd.DataFrame(data=data).T
-            df.columns = ['Davies-Bouldin index', 'silhouette_score', 'noise_ratio']
+            df.columns = ['Davies-Bouldin index', 'Silhouette score', 'Noise Ratio', 'Normalized Entropy', 'NN Ratio Score'] # , 'NN Ratio Score'
+#             df.drop('NN Ratio Score', axis=1, inplace=True)
             df.sort_index(ascending=False, inplace=True)
-            if df['noise_ratio'].sum() == 0.:
-                df.drop('noise_ratio', axis=1, inplace=True)
-            plt.xlabel('# estimated clusters')
-            label = label + '_' + method if len(method) > 0 else label
-            plt.title(suptitle + '_' + label if suptitle is not None else label)
-            df.plot.bar(ax=plt.gca())
+#             colors = ['blue', 'orange', 'green', 'red', 'purple']
+            colors = ['tab:blue', 'tab:orange', 'tab:green', 'tab:red', 'tab:purple'] # 
+
+            if df['Noise Ratio'].sum() == 0.:
+                df.drop('Noise Ratio', axis=1, inplace=True)
+                colors = ['tab:blue', 'tab:orange', 'tab:red', 'tab:purple'] # 
+            plt.xlabel('# estimated clusters', fontsize=FONTSIZE)
+            dataset = label.split('_')[0]
+            tax = label.split('_')[-1]
+            tax = tax.capitalize()
+            if dataset == 'ptb':
+                dataset = 'HMP'
+            label = f'Dataset: {dataset}, Tax: {tax}'
+            label = label + '\n' + method if len(method) > 0 else label
+            plt.title(suptitle + ', ' + label if suptitle is not None else label, fontsize=FONTSIZE)
+#             plt.plot(df.index.to_list(), [DB_THRESHOLD_DISTINCT]*df.shape[0], linestyle='--', color='blue')
+            plt.hlines(DB_THRESHOLD_DISTINCT, -1, max(df.index), linestyle='--', color='blue', alpha=0.5, label='DB index threshold')
+            plt.hlines(SILHOETTE_THRESHOLD_DISTINCT, -1, max(df.index), linestyle='--', color='orange', alpha=0.5, label='Silhoette score threshold')
+            plt.legend(fontsize=6)
+            df.plot.bar(ax=plt.gca(), color=colors)
 
             if data_default_metrics is not None:
                 def_clust_type, Silhoette_default = data_default_metrics[label]
     #                         if not DB_default > 1.5*max(df['Davies-Bouldin index']):
     #                             ax.hlines(DB_default, 0, len(df.index), linestyles='dotted', colors='blue')
                 ax.hlines(Silhoette_default, 0, len(df.index), linestyles='dotted', colors='orange', label=def_clust_type)
-                ax.legend()
+                ax.legend(fontsize=6)
             plt.show()
 
 # sample the data via binomial mask
@@ -264,23 +420,24 @@ def get_threshold_eigennumbers(pca, data, threshold=0.99, plot=False):
             ev_num = i
     #plot EV/CEVs
     if plot:
-        fig = plt.figure(figsize=(12,5.25))
+        fig = plt.figure(figsize=(10,5), dpi=DPI)
 
-        plt.subplot(121)
-        plt.title("Explained variance")
-        plt.xlabel("# PCs")
-        plt.grid(linestyle="dotted")
-        plt.plot(EV, "o-")
+#         plt.subplot(121)
+#         plt.title("Explained variance", fontsize=FONTSIZE)
+#         plt.xlabel("Number of PCs", fontsize=FONTSIZE)
+#         plt.grid(linestyle="dotted")
+#         plt.plot(EV, "o-")
 
-        plt.subplot(122)
-        plt.title("Cumulative explained variance")
-        plt.axhline(linewidth=1, y=0.99, color='r')
-        plt.axhline(linewidth=1, y=0.95, color='r')
-        plt.axhline(linewidth=1, y=0.9, color='r')
-        plt.axhline(linewidth=1, y=0.8, color='r')
-        plt.xlabel("# PCs")
+# #         plt.subplot(122)
+        plt.title("Cumulative explained variance", fontsize=FONTSIZE)
+#         plt.axhline(linewidth=1, y=0.95, color='r')
+#         plt.axhline(linewidth=1, y=0.9, color='r')
+#         plt.axhline(linewidth=1, y=0.8, color='r')
+        plt.xlabel("Number of PCs", fontsize=FONTSIZE)
         plt.grid(linestyle="dotted")
         plt.plot(CEV, "o-")        
+        plt.axhline(linewidth=1, y=0.99, color='r', label='99% of explained variance')
+        plt.legend(fontsize=FONTSIZE)
             
     return ev_num
     
@@ -292,7 +449,7 @@ def project(data, plot=False):
     pca_proj = PCA(n_components=ev_num,svd_solver='full',random_state=42)
     data_projected = pca_proj.fit_transform(data)
     if plot:
-        print('EV_NUM',ev_num,"REC_ERROR:",np.abs(pca_proj.inverse_transform(data_projected) - data_centered).mean())
+        print('EV_NUM',ev_num,"REC_ERROR:", mae_score(pca_proj.inverse_transform(data_projected), data_centered))
     return data_projected, pca_proj
 
 
@@ -305,3 +462,51 @@ def NPR(X, Z, k=21):
         npr += np.intersect1d(neigborhood_X[i], neigborhood_Z[i]).shape[0]
     npr_normed = npr / (k * n)
     return npr_normed
+
+
+
+def transform(method, X, dim, parameters, scorer):
+    try:
+        model_inst = method(n_components=dim, **parameters, n_jobs=-1)
+        Z = model_inst.fit_transform(X)
+    except:
+        print(f'ERROR for {parameters}')
+    if np.isnan(Z).any():
+        print(f'NAN for {parameters}')
+        return None
+    else:
+        return scorer(X,Z)
+
+
+def cross_validate(X,Z):
+    knn = KNeighborsRegressor()
+    mo = MultiOutputRegressor(knn)
+    mae_scorer = make_scorer(mae_score, greater_is_better=False)
+    return -cross_val_score(mo, Z, X, scoring=mae_scorer, cv=3, n_jobs=-1).mean()
+
+
+
+def calculate_Q_mae(X, Z):
+    
+    Q = coranking_matrix(X, Z)
+    
+    m = X.shape[0]
+    UL_cumulative = 0 
+    Q_k = []
+    LCMC_k = []
+    for k in range(0, Q.shape[0]):
+        r = Q[k:k+1,:k+1].sum()
+        c = Q[:k,k:k+1].sum()
+        UL_cumulative += (r+c)
+        Qnk = UL_cumulative/((k+1)*m) # (k+1)
+        Q_k.append(Qnk)
+        LCMC_k.append(Qnk - ((k+1)/(m-1)))
+    
+    argmax_k = np.argmax(LCMC_k)
+    k_max = np.arange(1.,m)[argmax_k]
+    Q_loc = (1./k_max)*np.sum(Q_k[:argmax_k+1])
+    Q_glob = (1./(m-k_max))*np.sum(Q_k[argmax_k+1:])
+    
+    mae = cross_validate(X, Z)
+    
+    return Q_loc, Q_glob, mae
